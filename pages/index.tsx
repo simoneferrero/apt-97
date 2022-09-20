4c12ef864eb4251e2dd86d6a887477424c54e04d
Simone Ferrero <simone.ferrero@outlook.com>
2022-09-20 19:27:41 +0100

    Fixed mobile height issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Container from '../components/container'
-import Layout from '../components/layout'
 import Head from 'next/head'
 import Image from 'next/image'
 import foodPic from '../public/images/food.jpg'
@@ -10,6 +9,7 @@ import {
   faUtensils,
   faMartiniGlassCitrus,
 } from '@fortawesome/free-solid-svg-icons'
+import useWindowSize from '../hooks/useWindowSize'
 
 type IconProps = {
   type: 'food' | 'drinks'
@@ -23,7 +23,7 @@ const Icon = ({ type, show }: IconProps) => (
         : show === 'portrait'
         ? 'landscape:!hidden'
         : ''
-    } text-7xl`}
+    }`}
   >
     <FontAwesomeIcon
       icon={type === 'food' ? faUtensils : faMartiniGlassCitrus}
@@ -32,15 +32,15 @@ const Icon = ({ type, show }: IconProps) => (
 )
 
 const Home = () => {
+  useWindowSize()
+
   return (
     <>
       <Head>
         <title>My Recipe Book</title>
       </Head>
-      <header>
-        <h1 className="absolute w-full pt-16 pb-16 text-7xl md:text-8xl font-bold font-cursive tracking-tighter leading-tight text-center select-none pointer-events-none z-40 text-teal-400 drop-shadow-lg drop-shadow-title">
-          My Recipe Book
-        </h1>
+      <header className="absolute w-full full-height-mobile pt-16 pb-16 text-7xl md:text-8xl font-bold font-cursive tracking-tighter leading-tight text-center select-none pointer-events-none z-40 text-teal-400 drop-shadow-lg drop-shadow-title flex portrait:items-center justify-center">
+        <h1>My Recipe Book</h1>
       </header>
       <Container>
         <Link href="/food">
@@ -54,9 +54,9 @@ const Home = () => {
                 objectPosition="center"
               />
             </div>
-            <div className="flex flex-col landscape:w-full landscape:h-screen portrait:w-screen portrait:h-full items-center landscape:justify-end portrait:justify-start select-none pointer-events-none text-center z-40 text-teal-700 drop-shadow-lg drop-shadow-title landscape:pb-7">
+            <div className="flex flex-col landscape:w-full full-height-mobile portrait:w-screen portrait:h-full items-center landscape:justify-end portrait:justify-start select-none pointer-events-none text-center z-40 text-teal-700 drop-shadow-lg drop-shadow-title text-5xl lg:text-7xl gap-2 md:gap-6 p-20">
               <Icon type="food" show="landscape" />
-              <h2 className="relative w-full pt-8 pb-8 text-7xl md:text-7xl font-bold font-body tracking-tighter leading-tight">
+              <h2 className="relative w-full font-bold font-body tracking-tighter leading-tight">
                 Our food
               </h2>
               <Icon type="food" show="portrait" />
@@ -74,9 +74,9 @@ const Home = () => {
                 objectPosition="center"
               />
             </div>
-            <div className="flex flex-col landscape:w-full landscape:h-screen portrait:w-screen portrait:h-full items-center justify-end select-none pointer-events-none text-center z-40 text-teal-700 drop-shadow-lg drop-shadow-title landscape:pb-7">
+            <div className="flex flex-col landscape:w-full full-height-mobile portrait:w-screen portrait:h-full items-center justify-end select-none pointer-events-none text-center z-40 text-teal-700 drop-shadow-lg drop-shadow-title text-5xl lg:text-7xl gap-2 md:gap-6 p-20">
               <Icon type="drinks" show="always" />
-              <h2 className="relative w-full pt-8 pb-8 text-7xl md:text-7xl font-bold font-body tracking-tighter leading-tight">
+              <h2 className="relative w-full font-bold font-body tracking-tighter leading-tight">
                 Our drinks
               </h2>
             </div>
