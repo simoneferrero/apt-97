2ef923de15394c34f5200c419c2d455b78486851
Simone Ferrero <simone.ferrero@outlook.com>
2022-10-02 21:57:11 +0100

    Added jest and testing library

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}
+
+module.exports = createJestConfig(customJestConfig)
